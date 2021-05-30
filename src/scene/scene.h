e6b7163c6044b77ba505891fb9f6ef0a010c8ee8
Chandradeep Pokhariya <chinu.deep100@gmail.com>
2021-05-30 14:16:08 +0530

    added the shader initialization

--- src/scene/scene.h
+++ src/scene/scene.h
@@ -4,14 +4,15 @@
 #include <stdio.h>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include "../shader/shader.h"
 
 class Scene{
     public:
         Scene();
         bool init(const int = 800, const int = 600);
-        bool should_close();
-        void swap_buffers();
-
+        inline bool should_close();
+        inline void swap_buffers();
+        void render();
     
     private:
         GLFWwindow *main_window;
